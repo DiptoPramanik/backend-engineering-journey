4cf807d8f76bd6528cbfb67f60e5831b48d9ef33
DiptoPramanik <diptopramanikprogramming05@gmail.com>
2026-06-05 20:54:08 +0600

    Encapsulation

--- 03-oop-in-csharp/Program.cs
+++ 03-oop-in-csharp/Program.cs
@@ -1,1 +1,104 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*using System;
+
+public class Auth
+{
+    public bool IsAuthenticated()
+    {
+        return true;
+    }
+}
+
+//Class definition
+public class Student
+{
+    //properties of Student class
+    // public string name;
+    // public int age;
+    //Encapsulation example                     
+    private string name;
+    private int age;
+    //Constructor Declaration
+    // public Student(string namep,int agep)
+    // {
+    //     name = namep;
+    //     age = agep;
+    // }
+    //another way to Constructor Declaration
+   /* public Student(string namep,int agep)
+    {
+        this.name = namep;
+        this.age = agep;
+    }*/
+    /*Auth auth = new Auth();
+    public void SetName(string name)
+    {
+        if(auth.IsAuthenticated())
+        {
+            this.name = name;
+        }
+        else
+        {
+            Console.WriteLine("You are not Authenticated");     
+        }
+        
+    }
+    public string GetName()
+    {
+        return this.name;
+    }
+    public void SetAge(int age)
+    {
+        if(auth.IsAuthenticated())
+        {
+            this.age = age;
+        }
+        else
+        {
+            Console.WriteLine("You are not Authenticated");     
+        }
+    }
+    public int GetAge()
+    {
+        return this.age;
+    }
+    //functionality of Student class
+    public void ShowInfo()
+    {
+        Console.WriteLine("Name : {0}, Age:{1}",name,age);
+    }
+}
+class Program
+{
+    //object oriented programming
+    //student management System
+    //Student - Name,Roll no,Marks,Courses - Properties
+    //Registration,Course Enrollment,Quiz,contest - functionality
+
+    public static void Main()
+    {
+        //object define
+        //Student student = new Student();
+        //student.name = "Dipto";
+        //student.age = 24;
+
+        // another way to declare
+        // Sudent student = new Student("Dipto",24);
+        //student.name = "pramanik";// can't update cuz private.o we need setter nd getter
+        Student student = new Student();
+        student.SetName("John");
+        student.SetAge(27);
+        student.ShowInfo();
+        //Console.WriteLine("Name : {0}, Age : {1}",student.GetName(),student.GetAge());
+    }
+}
+*/
+// Inheritance
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
