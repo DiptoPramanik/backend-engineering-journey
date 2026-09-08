dc7ae7db7ddc8ca8e7ce588e36c69018ed9071b8
DiptoPramanik <diptopramanikprogramming05@gmail.com>
2026-09-08 21:15:19 +0600

    added

--- 03-oop-in-csharp/obj/Debug/net10.0/03-oop-in-csharp.AssemblyInfo.cs
+++ 03-oop-in-csharp/obj/Debug/net10.0/03-oop-in-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("03-oop-in-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec565ed05479635e1db75491acb349c63a570f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93927ed2913848af564ad581457bb69c57b05a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("03-oop-in-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("03-oop-in-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
